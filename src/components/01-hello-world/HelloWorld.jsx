35bb7d1ec67f1fcafc278f7cd96a16c79e07cf24
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-02-26 17:02:02 +0300

    "react lesson day01 hello world "

---
+++ src/components/01-hello-world/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HelloWorld = () => {
+    return (
+        <>
+            <div>HelloWorld</div>
+            <div>HelloWorld</div>
+        </>
+    );
+};
+
+export default HelloWorld;
---
+++ src/components/01-hello-world/MyApp.jsx
@@ -0,0 +1,23 @@
+/* import React from 'react'
+
+const MyApp = () => {
+  return (
+    <div>Elma</div>
+  )
+}
+
+export default MyApp */
+
+import React from 'react';
+
+const MyApple = () => {
+    return <div>MyApple</div>;
+};
+
+export { MyApple };
+
+const MyFruit = () => {
+    return <div>MyFruit</div>;
+};
+
+export default MyFruit;
