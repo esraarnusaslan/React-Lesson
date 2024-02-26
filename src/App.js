6249c0316b6bd96c496914a050185d01d89c3649
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-02-26 21:02:29 +0300

    "short circuit"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import Jsx3ShortCircuit from "./components/02-jsx/Jsx3ShortCircuit.jsx";
-
 /*
+import Jsx3ShortCircuit from "./components/02-jsx/Jsx3ShortCircuit.jsx";
 import ControlMenu from "./components/02-jsx/ControlMenu";
 import React from 'react';
 import Jsx2 from './components/02-jsx/Jsx2';
- import MyReact from './components/01-hello-world/MyReact';
+import MyReact from './components/01-hello-world/MyReact';
 import Jsx1 from './components/02-jsx/Jsx1.jsx';
 import HelloWorld from './components/01-hello-world/HelloWorld.jsx'; 
 import MyFruit, { MyApple } from './components/01-hello-world/MyApp.jsx';
@@ -20,8 +19,8 @@ const App = () => {
                     <Jsx1 />
                     <Jsx2 />
                     <ControlMenu />
+                    <Jsx3ShortCircuit />
             */}
-            <Jsx3ShortCircuit />
         </div>
     );
 };
