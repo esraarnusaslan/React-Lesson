4cdf2b2d5429e5b933d844f4ad9aa1c2037f4310
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-03-07 12:28:45 +0300

    "link added to header"

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header>
             <h1>React Examples</h1>
+            <nav>
+                <Link to="/">Home</Link>
+            </nav>
         </header>
     );
 };
